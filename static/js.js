c0e1f6601dae54060c4ae2d7e3d0e6ab0e456ba8
Shyaboi <ian.shane.sears@gmail.com>
2020-09-20 16:12:36 -0700

    charts updatings some data, just gotta make it mine

--- static/js.js
+++ static/js.js
@@ -1,13 +1,93 @@
+var derta = [325, 333, 533, 452, 222, 344, 554, 366, 454, 634]
 
-  
-$( "#butt" ).click(()=> {
-    var str = $("#inpuut").val();
-    $.post("/", {"myData": str},(data)=> {
-    // $.get( "http://localhost:5000", function( data ) {
-        $.each(data, function( index, value ) {
-            $( ".result" ).append(`<li>[${value}]:the Arrar change|| matrix itteration index#: ${index}</li>`);
+var dertLen = derta.length
+
+// $( "#butt" ).click(()=> {
+  //   var str = $("#inpuut").val();
+  //   $.post("/", {"myData": datts},(data)=> {
+    //       $.each(data, function( index, value ) {
+      //           $( ".result" ).append(`<li>[${value}] : Array itteration index#: ${index}</li>`);
+      //         });
+      //   // console.log(data)
+      //   console.log(str)
+      // });
+      // })
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'index', 'voltron', 'dinnfds', 'fdjkdsf'],
+          datasets: [{
+            label: '# of Votes',
+            data: derta,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(12, 159, 64, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+              
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        });
+        
+        
+        function removeData(chart) {
+          chart.data.labels.pop();
+          chart.data.datasets.forEach((dataset) => {
+            dataset.data.pop();
           });
-    // console.log(data)
-    console.log(str)
-  });
-})
+          chart.update();
+        }
+        
+        console.log(derta.length)
+        setTimeout(() => {
+          for (let i = 0; i < dertLen+dertLen; i++) {removeData(myChart)}  
+        }, 4000);
+        
+        
+        function addData(chart, label, data) {
+          chart.data.labels.push(label);
+          chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+          });
+          chart.update();
+        }
+        var dattss = [[12, 333, 3, 5, 2, 3,554,3,454,634],[13, 343, 33, 5, 44444444, 3,554,3,454,634],[325, 333, 533, 452, 222, 344,554,366,454,634],[2, 3, 3, 5, 2, 3,4,3,4,4]]
+   var two =  [2, 3, 3, 5, 2, 3,4,3,4,4]
+
+    setTimeout(() => {
+        for (let i = 0; i < two.length; i++) {
+
+          addData(myChart, 'things', two)
+          
+        }
+      },4000);
