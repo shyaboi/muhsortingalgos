56c29696dc42b1147fb6bc6bf23eade174cf6e8c
Shyaboi <ian.shane.sears@gmail.com>
2020-09-20 22:38:12 -0700

    cleaned up and commented serv.py

--- bubs.py
+++ bubs.py
@@ -3,29 +3,27 @@ import random
 
 # benix function for taking in the post arguments and doing the sorting magic, and spitting back an array of arrays
 def benix(txt,other):
-
+# decalre random list if no user list was entered
     randomlist = []
+    # random list gen for loop
     for i in range(0,int(other)):
         n = random.randint(1,30)
         randomlist.append(n)
-
-    print(randomlist)
-    print(other)
-
-
-
-
+# lost list alias to return at end of itterations
     lost = []
-
+# if its not blank, the array is the user entered numbers split to an arr
     if txt!='':
       list = [int(s) for s in txt.split()]
       pass
     else:
+      # if the user didn't enter any text, the slider determines the indices amount
       list = randomlist
+      # the ol' buubble sort this one taken from https://www.tutorialspoint.com/python_data_structure/python_sorting_algorithms.htm
     def bubblesort(list):
 # Swap the elements to arrange in order
         for iter_num in range(len(list)-1,0,-1):
             for idx in range(iter_num):
+              # making a temp list copy 
                 termp = list.copy()
                 lost.insert(len(lost), termp)
                 if list[idx]>list[idx+1]:
@@ -33,5 +31,5 @@ def benix(txt,other):
                     list[idx] = list[idx+1]
                     list[idx+1] = temp
     bubblesort(list)
+    # returning the array of arrays
     return lost
-# print(list)
