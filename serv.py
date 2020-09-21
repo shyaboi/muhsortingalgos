2e02a66fb112b25b437e98a7f24c3c1da7d0a694
Shyaboi <ian.shane.sears@gmail.com>
2020-09-21 00:57:19 -0700

    fixed some borks, cleaned up serve file a bit

--- serv.py
+++ serv.py
@@ -1,33 +1,40 @@
+from sortingAlgos.bLite import benixLite
+from sortingAlgos.bubs import benix
+import sys
+from flask_cors import CORS
+import time
+from flask import abort, Flask, redirect, url_for, render_template, Response, jsonify, request
 from flask import Flask
 app = Flask(__name__)
-from flask import abort, Flask, redirect, url_for, render_template, Response, jsonify, request
-import time
-from flask import Flask
-from flask_cors import CORS
-import sys
 sys.path.append('../')
-from sortingAlgos.bubs import benix
-from sortingAlgos.bLite import benixLite
 
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route('/bubbleSort', methods = ['POST'])
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~bubble sort routes
+# bubble sort itteration array
+@app.route('/bubbleSort', methods=['POST'])
 def receive_data():
-    return jsonify(benix(request.form['arrToSort'],request.form['randomIndices']))
-
-@app.route('/onlyFinalResult/bubbleSort', methods = ['POST'])
+    return jsonify(benix(request.form['arrToSort'], request.form['randomIndices']))
+# final result api route
+@app.route('/onlyFinalResult/bubbleSort', methods=['POST'])
 def onlyFinalResultbubbleSort():
     return jsonify(benixLite(request.form['arrToSort']))
-    # return 'thanks bruh'
-@app.route('/', methods = ['GET'])
+# bub sort get jinja page
+@app.route('/bubbleSort', methods=['GET'])
+def bubSort():
+    return render_template("bubsort.html")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~bubble sort routes
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Home Route
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route('/bubbleSort', methods = ['GET'])
-def bubSort():
-    return render_template("bubsort.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
